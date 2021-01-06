f528bab78a3cf2a4f02b952ed1a3a723b4103479
aya-alasmar <aya.asmar32@gmail.com>
2021-01-06 10:20:59 +0200

    edit the code and using arrays to store q and answers

--- js/app.js
+++ js/app.js
@@ -2,19 +2,25 @@
 
 var grade = 0;
 var msg1, msg2, msg3, msg4, msg5, msg6, msg7; //corectness nsg for each question
+var msgs=new Array();
+var q =new Array();
+var answers =new Array();
+var uanswers= new Array();
+
 var q1,q2,q3,q4,q5,q6,q7; // the answers 
 var correctMsg=" "; // to enter the msg that will shows in alert if the user guess the number 
 var colors = ["pink", "black", "white", "blue"]; // array for qeustion 7
 var numIteration =new Array(); ; //array to store the user number input for qeustion 6 (at most length=4)
 var colorIteration =new Array(); //array to store the user input for qeustion 7 (at most length=6)
+var ranNum =Math.floor(Math.random() * 10);
 
 
 
 //start of validation function to validate the y/n answers
 function validation(uinput, msg) {
-    while (uinput === '' || uinput === null || uinput === undefined || !(uinput.toLowerCase() === 'y' || uinput.toLowerCase() === 'n' ||uinput.toLowerCase() === 'no'||uinput.toLowerCase() === 'yes')) {
+    while (uinput === '' || uinput === null || uinput === undefined || !(uinput.toLowerCase() === 'y' || uinput.toLowerCase() === 'n')) {
         uinput = prompt(msg);
-        if (!(uinput.toLowerCase() === 'y' || uinput.toLowerCase() == 'n' || uinput.toLowerCase() === 'no' || uinput.toLowerCase() === 'yes')) {
+        if (!(uinput.toLowerCase() === 'y' || uinput.toLowerCase() == 'n' )) {
             alert("you can input Y / N or yes/no only ");
         }
     }
@@ -46,84 +52,49 @@ function validationColor(uinput, msg) {
 // end of validationColor function
 
 
-//questions and answers
-q1 = validation(q1, "My name is Ayat ? Y/N ");
-if (q1.toLowerCase() === 'n' ||q1.toLowerCase() === 'no') {
-    grade++;
-    msg1 = "correct";
-    alert(" correct , keep going! ");
+// the questions
+q[0]="My name is Ayat ? Y/N ";
+q[1]="I was graduated form Zarqa University? Y/N ";
+q[2]="I'm a programmer ? Y/N  ";
+q[3]="I'm intrested in frontend ? Y/N ";
+q[4]="I have 2 years work experience? Y/N";
+
+
+// the coorect answers 
+answers[0]='n';
+answers[1]='y';
+answers[2]='y';
+answers[3]='y';
+answers[4]='n';
+
+
+// to ask the qestions 1-5 
+for(var i=0 ;i<5;i++){
+    uanswers[i]=validation(uanswers[i],q[i]);
+    if (uanswers[i].toLowerCase() == answers[i].toLowerCase()) {
+        grade++;
+        msgs[i] = "correct";
+        alert(" correct , keep going! ");
+    
+    }
+    else {
+        msgs[i]= "incorect";
+        alert(" incorrect , don't worry! ");
+    }
+    console.log(q[i], uanswers[i], " it's " + msgs[i]);
 
 }
-else {
-    msg1 = "incorect";
-    alert(" incorrect , don't worry! ");
-}
-
-
-
-q2 = validation(q2, "I was graduated form Zarqa University? Y/N ");
-if (q2.toLowerCase() === 'y' ||q2.toLowerCase() === 'yes') {
-    grade++;
-    msg2 = "correct";
-    alert(" correct , keep going! ");
-}
-else {
-    msg2 = "incorect";
-    alert(" incorrect , don't worry! ");
-}
-
-
-
-q3 = validation(q3, "I'm a programmer ? Y/N ");
-if (q3.toLowerCase() === 'y' ||q3.toLowerCase() === 'yes') {
-    grade++;
-    msg3 = "correct";
-    alert(" correct , keep going! ");
-
-}
-else {
-    msg3 = "incorect";
-    alert(" incorrect , don't worry! ");
-
-}
-
-
-
-q4 = validation(q4, "I'm intrested in frontend ? Y/N ");
-if (q4.toLowerCase() === 'y' || q4.toLowerCase() === 'yes') {
-    grade++;
-    msg4 = "correct";
-    alert(" correct , keep going! ");
-
-}
-else {
-    msg4 = "incorect";
-    alert(" incorrect , don't worry! ");
-}
-
-
- q5 = validation(q5, "I have 2 years work experience? Y/N ");
-if (q5.toLowerCase() === 'n' || q5.toLowerCase() === 'no') {
-    grade++;
-    msg5 = "correct";
-    alert(" correct , keep going! ");
-}
-else {
-    msg5 = "incorect";
-    alert(" incorrect , don't worry! ");
-}
-
 
 
 // for question number 6
 for (var i = 4; i > 0; i--) {
-    q6 = validationNum(q6, "guess my fav number  ^_^ , you still have "+ i +"  iteration ");
-    if (parseInt(q6) < 8) {
+    q6 = validationNum(q6, "choose a random ineger number between 1-100 ^_^ , you still have "+ i +"  iteration ");
+    if (parseInt(q6) < ranNum) {
         msg6 = "too low!";
         numIteration.push(q6);
         alert(msg6);
     }
-    else if (parseInt(q6) > 8) {
+    else if (parseInt(q6) > ranNum) {
         msg6 = "too high!";
         numIteration.push(q6);
         alert(msg6); 
@@ -138,7 +109,8 @@ for (var i = 4; i > 0; i--) {
 
 }
 alert ("the numbers you are already chosen "+numIteration.toString());
-alert(correctMsg+" my fav number is 8 *_*");
+alert(correctMsg+" the random number is " + ranNum);
+console.log(correctMsg,"the random number is " , ranNum);
 
  
 
@@ -163,13 +135,7 @@ for (var i = 6; i > 0; i--) {
 
 } 
 alert("your color iteration : "+colorIteration.toString() + " and my fav colors are "+colors.toString());
+console.log("your color iteration : ", colorIteration.toString() , " and my fav colors are " ,colors.toString());
 
 
-
-// console screen
-console.log("My name is Ayat ? you answered", q1, " it's " + msg1);
-console.log("I was graduated form Zarqa University? you answered", q2, " it's " + msg2);
-console.log("I'm a programmer ? you answered", q3, " it's " + msg3);
-console.log("I'm intrested in frontend ? you answered", q4, " it's " + msg4);
-console.log("I have 2 years work experience? you answered", q5, " it's " + msg5);
-alert(" your grade : " + grade);
+alert(" your grade : " + grade); 
